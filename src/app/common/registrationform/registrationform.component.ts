04cd9449cb7d9a6fc334ef573318674d2ee3f190
Kaushik Katariya <117052909+Kaushik2710@users.noreply.github.com>
2024-03-13 22:35:09 +0530

    registration

---
+++ src/app/common/registrationform/registrationform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrationform',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrationform.component.html',
+  styleUrl: './registrationform.component.css'
+})
+export class RegistrationformComponent {
+
+}
