06c9de770bbd574861c0d0885ac940a7fc311e34
kkumarharsh200 <kkumarharsh200@gmail.com>
2024-03-14 01:41:48 +0530

    loginPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink, RouterModule, RouterOutlet} from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import { SidebarComponent } from './Common/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
 import { RegistrationformComponent } from './common/registrationform/registrationform.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, SidebarComponent,RouterLink,CommonModule,RegistrationformComponent,RouterModule],
+  imports: [RouterOutlet, MatButtonModule, SidebarComponent ,RouterLink,CommonModule,RegistrationformComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/common/loginform/loginform.component.ts
+++ src/app/common/loginform/loginform.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, ReactiveFormsModule, FormControl } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loginform',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,  
+    MatFormFieldModule, 
+    MatInputModule, 
+    CommonModule, 
+    MatButtonModule
+  ],
   templateUrl: './loginform.component.html',
   styleUrl: './loginform.component.css'
 })
-export class LoginformComponent {
+export class LoginformComponent implements OnInit{
+
+  loginForm !: FormGroup;
+
+  constructor(private fb: FormBuilder){};
+
+  ngOnInit(): void {
+      this.loginForm = this.fb.group({
+        username: [''],
+        password: new FormControl(''),
+      })
+  }
+
+  check(){
+    console.log(this.loginForm.value)
+    if(this.loginForm.value.username === "admin@gmail.com" && this.loginForm.value.password ==="admin"){
+      console.log("Login Successful");
+    }
+    else{
+      console.log("Try again");
+    }
+    this.loginForm.reset();
+  }
 
 }
