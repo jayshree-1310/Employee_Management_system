cf3676d455a7ce44b655dd80cff517359952550a
AshishRaj07 <ashishrajfdmr@gmail.com>
2024-03-14 01:32:08 +0530

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterModule, RouterOutlet} from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { SidebarComponent } from './Common/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
@@ -9,10 +9,12 @@ import { RegistrationformComponent } from './common/registrationform/registratio
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, SidebarComponent,RouterLink,CommonModule,RegistrationformComponent],
+  imports: [RouterOutlet, MatButtonModule, SidebarComponent,RouterLink,CommonModule,RegistrationformComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'EMS';
+  
 }
+
