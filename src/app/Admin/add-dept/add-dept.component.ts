70ccb23d2a0a51b40056b070933bbb84699b27b7
Adarsh Ranjan Tripathy <apjindia2030@gmail.com>
2024-03-19 00:42:43 +0530

    Update add-dept.component.ts

--- src/app/Admin/add-dept/add-dept.component.ts
+++ src/app/Admin/add-dept/add-dept.component.ts
@@ -1,12 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-add-dept',
-  standalone: true,
-  imports: [],
-  templateUrl: './add-dept.component.html',
-  styleUrl: './add-dept.component.css'
-})
-export class AddDeptComponent {
-
-}
