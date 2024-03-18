ebe1ec48b9f3aa3683f3c44ec981dec6169a271e
Adarsh Ranjan Tripathy <apjindia2030@gmail.com>
2024-03-18 21:14:30 +0530

    Update apply-leave.component.ts

--- src/app/Employee/apply-leave/apply-leave.component.ts
+++ src/app/Employee/apply-leave/apply-leave.component.ts
@@ -2,24 +2,7 @@ import { Component } from '@angular/core';
 import {  FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LeaveService } from '../../core/leave.service';
 
-@Component({
-  selector: 'app-apply-leave',
-  standalone: true,
-  imports: [ReactiveFormsModule],
-  templateUrl: './apply-leave.component.html',
-  styleUrl: './apply-leave.component.css'
-})
-export class ApplyLeaveComponent {
-  leaveForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private leaveService: LeaveService) {
-    this.leaveForm = this.formBuilder.group({
-      leaveType: ['', Validators.required],
-      startDate: ['', Validators.required],
-      endDate: ['', Validators.required],
-      reason: ['', Validators.required]
-    });
-  }
 
   onSubmit() {
     if (this.leaveForm.invalid) {
