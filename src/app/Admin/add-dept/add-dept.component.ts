6171c8551f3625890ddb76e76eb08b6affcb8806
Kaushik2710 <katariyakaushik271@gmail.com>
2024-03-19 10:56:43 +0530

    add department complete

--- src/app/Admin/add-dept/add-dept.component.ts
+++ src/app/Admin/add-dept/add-dept.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-dept',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-dept.component.html',
   styleUrl: './add-dept.component.css'
 })
 export class AddDeptComponent {
-
+  onInputChange() {
+    throw new Error('Method not implemented.');
+  }
+  addDepartment() {
+    console.log(['/add-dept'])
+    window.alert("Department Added Successfully")
+  }
+  department: number = 0;
 }
 
