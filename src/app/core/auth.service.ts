5efea9c9f41a1e6b1bde285c7e368a719eb3c5d1
Adarsh Ranjan Tripathy <apjindia2030@gmail.com>
2024-03-19 11:20:47 +0530

    Update auth.service.ts

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -6,8 +6,7 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class AuthService {
   constructor() {}
-  public isAdmin = new BehaviorSubject<boolean>(false);
-  public isEmployee = new BehaviorSubject<boolean>(false);
+  
   logoutAdmin() {
     this.isAdmin.next(false);
   }
