aead09a5b0aa03cc18a674363a982bb10dc3d561
AshishRaj07 <ashishrajfdmr@gmail.com>
2024-04-24 00:45:42 +0530

    changes in salary-slip

--- src/app/Employee/salaryslip/salaryslip.component.ts
+++ src/app/Employee/salaryslip/salaryslip.component.ts
@@ -3,16 +3,23 @@ import { RouterLink } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { EmployeeService } from '../../core/employee-service.service';
 import { AuthService } from '../../core/auth.service';
+import { MatCardModule } from '@angular/material/card';
+
+
+
 @Component({
   selector: 'app-salaryslip',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,
+            MatCardModule,
+            
+  ],
   templateUrl: './salaryslip.component.html',
   styleUrl: './salaryslip.component.css'
 })
 export class SalaryslipComponent {
   employee: any; // Define the employee object to hold employee data
-
+  
   constructor(private authService: AuthService,
     private employeeService: EmployeeService,
   ) { }
