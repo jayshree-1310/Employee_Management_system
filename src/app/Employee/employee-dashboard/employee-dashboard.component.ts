0215bb347ecd94b1f95d30ad67e271f68d5944e2
Arunav mishra <arrrnav7@gmail.com>
2024-04-23 10:51:51 +0530

    Update on Employee Dashboard component

--- src/app/Employee/employee-dashboard/employee-dashboard.component.ts
+++ src/app/Employee/employee-dashboard/employee-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { OnInit } from '@angular/core';
+import { EmployeeService } from '../../core/employee-service.service';
+import { AuthService } from '../../core/auth.service';
 @Component({
   selector: 'app-employee-dashboard',
   standalone: true,
@@ -9,5 +11,19 @@ import { RouterLink } from '@angular/router';
   styleUrl: './employee-dashboard.component.css'
 })
 export class EmployeeDashboardComponent {
+  employee: any; // Define the employee object to hold employee data
 
+  constructor(private authService: AuthService,
+    private employeeService: EmployeeService,
+  ) { }
+
+ngOnInit(): void {
+  // Fetch employee data including salary
+  this.employeeService
+  .getEmployeeByEmail(localStorage.getItem('email'))
+  .subscribe((data) => {
+    this.employee = data;
+  });
 }
+}
+
